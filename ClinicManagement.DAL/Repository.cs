1df651d2391c6a5e60197530936ea692d59b5ee6
Dip <dip@3sixtyit.com>
2021-05-21 17:24:21 +0530

    Add project files.

---
+++ ClinicManagement.DAL/Repository.cs
@@ -0,0 +1,44 @@
+﻿using ClinicManagement.IDAL;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinicManagement.DAL
+{
+    public class Repository<T> : IRepository<T> where T : class 
+    {
+        private DbContext ClinicDAL = null;
+        private DbSet<T> dbentity = null;
+        public void Add(T obj)
+        {
+            this.ClinicDAL.Set<T>().Add(obj);
+        }
+
+        public void Delete(T obj)
+        {
+           this.ClinicDAL.Set<T>().Remove(obj);
+        }
+
+        public T GetById(int FindId)
+        {
+            return  ClinicDAL.Set<T>().Find(FindId);
+        }
+
+        public IQueryable<T> GetAll()
+        {
+             return ClinicDAL.Set<T>().AsNoTracking();
+        }
+
+        public void Save()
+        {
+            ClinicDAL.SaveChanges();
+        }
+
+        public void Update(T obj)
+        {
+            this.ClinicDAL.Set<T>().Update(obj);
+        }
+    }
+}
---
+++ ClinicManagement.IDAL/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ClinicManagement.IDAL
+{
+    public interface IRepository<T> where T : class
+    {
+        void Add(T obj);
+        void Update(T obj);
+        void Delete(T obj);
+        IQueryable<T> GetAll();
+        T GetById(int FindId);
+        void Save();
+
+    }
+}
