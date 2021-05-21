0630312c1c2ae767da67644ec4f5d63178453b24
Dip <dip@3sixtyit.com>
2021-05-21 18:11:31 +0530

    Commit New Classes

---
+++ ClinicManagement.EF/Entity/Gender.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClinicManagement.EF.Entity
+{
+    public enum Gender
+    {
+        [Description("Male")]
+        Male = 1,
+
+        [Description("Female")]
+        Female
+    }
+}
